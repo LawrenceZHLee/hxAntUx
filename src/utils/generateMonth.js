bb591254a8ed62d47273e1db1d03e9d02dfcd8ad
zilan830 <zilan830@163.com>
2018-05-28 16:16:15 +0800

    日期格式更改

--- src/utils/generateMonth.js
+++ src/utils/generateMonth.js
@@ -3,8 +3,8 @@ import moment from 'moment';
 //生成时间
 const month = (result,size,d) => {
   if(size === 0) return result;
-  const newMonth=moment().subtract(size, 'M').format('L');
-  result.push(newMonth.slice(2));
+  const newMonth=moment().subtract(size, 'M').format('LL');
+  result.push(newMonth.match(/.+月/)[0]);
   return month(result,size-1,d);
 };
 
